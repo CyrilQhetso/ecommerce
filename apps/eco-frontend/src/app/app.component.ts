0341ff4ae12f59de54c8115aca7fed9d1389759d
CyrilQhetso <cyril.qhetso@gmail.com>
2025-03-31 16:42:46 +0200

    Ecommerce base-structure

--- apps/eco-frontend/src/app/app.component.ts
+++ apps/eco-frontend/src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { FaConfig, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  standalone: true,
+  imports: [RouterModule, NavbarComponent, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'eco-frontend';
+export class AppComponent implements OnInit{
+  
+  private faIconLibrary: FaIconLibrary;
+  private faConfig : FaConfig;
+
+  constructor(faIconLibrary: FaIconLibrary, faConfig: FaConfig) {
+    this.faIconLibrary = faIconLibrary;
+    this.faConfig = faConfig;
+  }
+
+  ngOnInit(): void {
+      this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
